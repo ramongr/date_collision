899f5aa387abee20ff5ddf2b59ba421f4d2a8b3a
Ramon Rodrigues <cerberus.ramon@gmail.com>
2017-08-21 15:43:41 +0100

    DateCollision changed methods

--- date_collision.js
+++ date_collision.js
@@ -1,3 +1,19 @@
+/*
+  Date collision detection
+  There's an initial date range. Date collision will check if another date range collides with it and then calculates
+  which one of 3 things should happen to the original date range:
+  Split:
+    - The new date range has a start and end date between the original date range, the original must be split.
+  Override (head):
+    - The new date range has a start date that's equal or before the original date range and the new date range end date
+      is contained in the original date range, the original start date should be changed.
+  Override (tail):
+    - Mutatis mutandis Override (head) start date for end date
+  Override (full):
+    - The new date range has both the original start date and end date within it's range. The original date range
+    will be deleted.
+*/
+
 function DateCollision(startDate, endDate){
   this.startDate = startDate;
   this.endDate = endDate;
@@ -7,14 +23,14 @@ function DateCollision(startDate, endDate){
 DateCollision.prototype.newDateRangeContains = function(startDate, endDate) {
   this.newDateCollideStart = moment(startDate).isBetween(this.startDate, this.endDate, null, '[]');
   this.newDateCollideEnd = moment(endDate).isBetween(this.startDate, this.endDate, null, '[]');
-  return this.newDateCollideStart && this.newDateCollideEnd;
+  return this.newDateCollideStart || this.newDateCollideEnd;
 };
 
 DateCollision.prototype.originalDateRangeContains = function(startDate, endDate) {
   this.originalDateCollideStart = moment(this.startDate).isBetween(startDate, endDate, null, '[]');
   this.originalDateCollideEnd = moment(this.endDate).isBetween(startDate, endDate, null, '[]');
 
-  return this.originalDateCollideStart && this.originalDateCollideEnd;
+  return this.originalDateCollideStart || this.originalDateCollideEnd;
 };
 
 DateCollision.prototype.calculateDates = function(startDate, endDate) {
@@ -35,23 +51,23 @@ DateCollision.prototype.calculateDates = function(startDate, endDate) {
 
 };
 
-function splitDate(startDate, endDate) {
+DateCollision.prototype.splitDate = function(startDate, endDate) {
   return [
     {startDate: this.startDate, endDate: startDate},
     {startDate: startDate, endDate: endDate},
     {startDate: endDate, endDate: this.endDate}
   ];
-}
+};
 
-function overrideHead(startDate, endDate) {
+DateCollision.prototype.overrideHead = function(startDate, endDate) {
   return [{startDate: startDate, endDate: endDate}, {startDate: endDate, endDate: this.endDate}];
-}
+};
 
-function overrideTail(startDate, endDate) {
+DateCollision.prototype.overrideTail = function(startDate, endDate) {
   return [{startDate: this.startDate, endDate: startDate},{startDate: startDate, endDate: endDate}];
-}
+};
 
 
-function validDateRange(startDate, endDate) {
+DateCollision.prototype.validDateRange = function(startDate, endDate) {
   return moment(startDate).isAfter(endDate);
-}
+};
